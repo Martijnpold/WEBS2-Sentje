4c919e8504658920d4525da2d9a3ffe52f2f6725
Martijnpold <martijntje7@gmail.com>
2019-03-13 02:54:40 +0100

    Add basic way of fetching database payment requests

--- sentje/app/Http/Controllers/HomeController.php
+++ sentje/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\PaymentRequest;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,18 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $user_id = Auth::user()->id;
+        $requests = PaymentRequest::where('owner_id', $user_id)->get();
+        echo '<table style="width:100%">';
+        foreach($requests as $request) {
+            echo '<tr>';
+            echo '<td>' . $request->id . '</td>';
+            echo '<td>' . $request->owner_id . '</td>';
+            echo '<td>' . $request->amount . '</td>';
+            echo '<td>' . $request->created_at . '</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
         return view('home');
     }
 }
