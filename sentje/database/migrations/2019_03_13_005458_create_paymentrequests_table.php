0c3202cf70e251dc781524aceda8c5f488b15d17
Martijnpold <martijntje7@gmail.com>
2019-03-13 02:29:48 +0100

    Add payment request model

---
+++ sentje/database/migrations/2019_03_13_005458_create_paymentrequests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentrequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('owner_id');
+            $table->decimal('amount');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_requests');
+    }
+}
